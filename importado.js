a84677b7fe40933f2d1f577301db7bbe3e6c39d6
FeFernando <12felipexd@gmail.com>
2019-04-17 04:30:02 -0300

    Initial commit

---
+++ importado.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-exibir-nome').addEventListener('click', function () {
+var nome1 = 'Gabriel';
+var nome2 = 'Daniel';
+var nome3 = 'Ronaldo';
+var nome4  ='Dennis Ritchie';
+
+alert('O primeiro nome tem, ' + nome1.length + ' letras!');
+alert('O segundo nome tem, ' + nome2.length + ' letras!');
+alert('O tercceiro nome tem, ' + nome3.length + ' letras!');
+alert('O quarto nome tem, ' + nome4.length + ' letras!');
+	// body...
+})
+
